96a8ea2f0dac86d839c4e3cd66d0d3aa78d4ad92
DinhTran <tranthanhdinh2000@gmail.com>
2021-11-17 15:22:56 +0700

    Add bubble sort method and finish

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Sorting_Algorithm.Bubble_Sort.BubbleSort;
+
+public class Main {
+    public static void main(String[] args) {
+        new BubbleSort();
+
+    }
+
+}
+
--- src/Sorting_Algorithm/Bubble_Sort/BubbleSort.java
+++ src/Sorting_Algorithm/Bubble_Sort/BubbleSort.java
@@ -13,9 +13,36 @@ public class BubbleSort {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100);
         }
+        System.out.println("Mang da tao ra: ");
+        printArray(arr);
+        System.out.println();
 
 
+        SortingByBubbleAlgorithm(arr);
 
+        System.out.println("Mang sau khi da sap xep: ");
+        printArray(arr);
+
+
+    }
+
+    private void SortingByBubbleAlgorithm(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+
+    private void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
 
